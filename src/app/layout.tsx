0db08c873e5c5ecfc741b4a9c6eb7771783951a4
Jorik.tsx <jorikvanruiswijk@gmail.com>
2025-01-10 13:48:37 +0100

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+import { PrismicPreview } from "@prismicio/next";
+import { repositoryName } from "@/prismicio";
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="bg-blue-400">{children}</body>
+      <PrismicPreview repositoryName={repositoryName} />
+    </html>
+  );
+}
