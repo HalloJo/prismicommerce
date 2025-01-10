a3105f978e8feb40907c534bcd320e49b91f2820
Jorik.tsx <jorikvanruiswijk@gmail.com>
2025-01-10 14:08:31 +0100

    Edit styling of RichText

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-blue-400">{children}</body>
-      <PrismicPreview repositoryName={repositoryName} />
+      <body className="flex flex-col items-center bg-slate-50">
+        <div className="bg-white max-w-7xl min-h-screen border-x border-solid border-gray-200 p-12 w-full flex flex-col gap-20 items-center text-slate-700">
+          {children}
+          <PrismicPreview repositoryName={repositoryName} />
+        </div>
+      </body>
     </html>
   );
 }
